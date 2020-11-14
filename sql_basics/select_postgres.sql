fc39fb52543865623a9b35b277cd0f817dd855e5
orlovskiy-artem <artem.orlovsky@gmail.com>
2020-11-14 14:41:47 +0200

    Fixed query to one select

--- sql_basics/select_postgres.sql
+++ sql_basics/select_postgres.sql
@@ -1,13 +1,8 @@
-SELECT first_name, last_name, total_orders, total_quantity, t3.total_money_spent
-FROM ((SELECT t.person_id, t.first_name, t.last_name, t.total_orders
-      FROM (SELECT p.id AS person_id, p.first_name, p.last_name, count(o.id) AS total_orders
-            FROM person p
-                     LEFT OUTER JOIN "order" o ON p.id = o.person_id
-            GROUP BY p.id, p.first_name, p.last_name) AS t) AS t2
-               LEFT OUTER JOIN
-           (SELECT person_id, sum(quantity) AS total_quantity,SUM((price - discount) * quantity) AS total_money_spent
-            FROM "order" o
-                     INNER JOIN order_item oi ON o.id = oi.order_id
-                     INNER JOIN item i ON oi.item_id = i.id
-            GROUP BY person_id) AS t3 ON t2.person_id = t3.person_id)
-ORDER BY total_orders;
+SELECT p.first_name,p.last_name,
+       count(DISTINCT order_id) AS total_orders,
+       sum(quantity) AS total_items_bought,
+       sum((price-discount)*quantity) AS total_money_spent
+       FROM person p  LEFT OUTER JOIN  "order" o ON p.id = o.person_id
+                      LEFT OUTER JOIN  order_item oi ON oi.order_id = o.id
+                      LEFT OUTER JOIN item i ON oi.item_id = i.id
+GROUP BY p.first_name,p.last_name;
